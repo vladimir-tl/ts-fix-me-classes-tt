c80323c94cc4442baa856cf4b9f0c37a8cb87608
vladimirovodenko <vladimir@tallinn-learning.ee>
2024-12-25 14:28:10 +0100

    test c

--- src/bank-account.ts
+++ src/bank-account.ts
@@ -2,7 +2,7 @@ class BankAccount {
     balance: number
 
     constructor(initialAmount: string) {
-        balance = initialAmount
+        this.balance = initialAmount
     }
 
     deposit(amount: number): number {
